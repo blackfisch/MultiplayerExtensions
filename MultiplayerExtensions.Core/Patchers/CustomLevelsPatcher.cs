0bbac02cb3137ae09c7902972296585be26892f6
MT <littlehappytroll@gmail.com>
2021-11-28 19:47:07 -0500

    state and start button enable

--- MultiplayerExtensions.Core/Patchers/CustomLevelsPatcher.cs
+++ MultiplayerExtensions.Core/Patchers/CustomLevelsPatcher.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine.UI;
 
 namespace MultiplayerExtensions.Core.Patchers
 {
@@ -35,6 +36,22 @@ namespace MultiplayerExtensions.Core.Patchers
             // make default 'all' songpackmask not include custom levels - this is for official matchmaking
             SongPackMask.TryParse((BitMask128.maxValue ^ "custom_levelpack_CustomLevels".ToBloomFilter<BitMask128>(3, 8)).ToString(), out __result);
             return false;
+        }
+
+        [AffinityPrefix]
+        [AffinityPatch(typeof(LobbySetupViewController), nameof(LobbySetupViewController.SetPlayersMissingLevelText))]
+        private void SetPlayersMissingLevelText(LobbySetupViewController __instance, string playersMissingLevelText, ref Button ____startGameReadyButton)
+        {
+            if (!string.IsNullOrEmpty(playersMissingLevelText) && ____startGameReadyButton.interactable)
+                __instance.SetStartGameEnabled(CannotStartGameReason.DoNotOwnSong);
+        }
+
+        [AffinityPostfix]
+        [AffinityPatch(typeof(MultiplayerSessionManager), "InitInternal")]
+        private void SessionManagerInitInternal(MultiplayerSessionManager __instance)
+        {
+            __instance.SetLocalPlayerState("modded", true);
+            // TODO: move this to a better place
         }
     }
 }

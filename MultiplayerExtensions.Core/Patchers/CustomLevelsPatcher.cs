8f3204ef042a335dca4912c07737a9403686c8c0
MT <littlehappytroll@gmail.com>
2021-12-06 08:24:00 -0500

    clean up a thing

--- MultiplayerExtensions.Core/Patchers/CustomLevelsPatcher.cs
+++ MultiplayerExtensions.Core/Patchers/CustomLevelsPatcher.cs
@@ -1,10 +1,5 @@
 ﻿using SiraUtil.Affinity;
 using SiraUtil.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine.UI;
 
 namespace MultiplayerExtensions.Core.Patchers
